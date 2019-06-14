fadc592b47b237ca9ccd69ac44f6bb7abe759c63
Zach Maas <zach.maas@gmail.com>
2019-06-14 07:46:23 -0600

    Tweaks to build.sh to get FStitch to successfully build.

--- packages/fstitch/build.sh
+++ packages/fstitch/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sed -i=.bak "s|g++|$GXX|g" src/Makefile
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include
+
+sed -i=.bak -e "s|g++|$GXX|g" src/Makefile
 bash setup.sh
-cp src/FStitch ./fstitch
+cp src/FStitch ${PREFIX}/bin/fstitch
